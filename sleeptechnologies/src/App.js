a08b2b33262b1691d0677c766ae7dabc7a917d70
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 12:19:58 +0500

    solved navbar isssue

--- sleeptechnologies/src/App.js
+++ sleeptechnologies/src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
    <>
-   <Navbar/>
+   <Router>
+  <Routes>
+    <Route exact path='/' element={<Navbar/>}/>
+  </Routes>
+    </Router>
    </>
     </div>
   );
--- sleeptechnologies/src/components/Navbar.jsx
+++ sleeptechnologies/src/components/Navbar.jsx
@@ -3,21 +3,30 @@ import "../styles/Navbar.css";
 import Logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+export default function  Navbar ()
+{
   return (
     <div className="navbar">
         <div className="leftside">
-
             <img src={Logo} alt='logo here'/>
         </div>
         <div className='rightside'>
         <Link to="/"> Home </Link>
         <Link to="/Brand"> Brand </Link>
-        <Link to="/Mission">Mission </Link>
+        <Link to="/Mission"> Mission </Link>
         <Link to="/Contact"> Contact</Link>
         </div>
     </div>
-  )
+  //   <div className="collapse navbar-collapse" id="navbarNav">
+  //   <ul className="navbar-nav">
+  //     <li className="nav-item">
+  //       <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+  //     </li>
+  //     <li className="nav-item">
+  //       <Link className="nav-link" to="/login">Login</Link>
+  //     </li>
+  //   </ul>
+  // </div>
+  );
 }
 
-export default Navbar;
