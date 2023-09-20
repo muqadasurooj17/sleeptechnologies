37e10adb2ef323dec7d90647b547f92f83bbdf33
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 10:36:38 +0500

    added react router dom

--- sleeptechnologies/src/components/Navbar.jsx
+++ sleeptechnologies/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Logo from "../assets/pizzaLogo.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='leftside'>
+            <img src={logo}/>
+        </div>
+        <div className='rightside'>
+            <Link to="/">Home</Link>
+            <Link to="/">Brand</Link>
+            <Link to="/">Mission</Link>
+            <Link to="/">Carrer</Link>
+            <Link to="/">Contact</Link>
+        </div>
+    </div>
   )
 }
 
