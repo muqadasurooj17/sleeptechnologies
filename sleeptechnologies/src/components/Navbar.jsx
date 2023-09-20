7115886ba9c203a25c06141e3e4be8675310191c
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 10:50:12 +0500

    added links

--- sleeptechnologies/src/components/Navbar.jsx
+++ sleeptechnologies/src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Navbar.css";
 import Logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className='leftside'>
+    <div className="navbar">
+        <div className="leftside">
+
             <img src={Logo} alt='logo here'/>
         </div>
         <div className='rightside'>
-            <Link to="/">Home</Link>
-            <Link to="/">Brand</Link>
-            <Link to="/">Mission</Link>
-            <Link to="/">Carrer</Link>
-            <Link to="/">Contact</Link>
+        <Link to="/"> Home </Link>
+        <Link to="/Brand"> Brand </Link>
+        <Link to="/Mission">Mission </Link>
+        <Link to="/Contact"> Contact</Link>
         </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
