311b49ce262f24dddde34531a86098863a47f1e8
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 20:25:38 +0500

    deleted

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Career from './components/Career';
 import myImage from './images/pexels-fox-1-min.jpg';
 import Brand from './components/Brand';
 import myImage2 from './images/pexels-fox-2-min.jpg';
+import Mission from './components/Mission';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import Mission from './components/Mission';
-import Career from './components/Career';
+
 
 function App() {
   return (
@@ -25,7 +24,7 @@ function App() {
 <Brand/>
 <Mission/>
 <img src={myImage2} alt="Description of the image" style={{width:"100%", height:"max-content"}}/>
-<Career/>
+
     </Router>
    </>
     </div>
