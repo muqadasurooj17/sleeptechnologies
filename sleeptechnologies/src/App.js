bc71f4b7dbfbba92a930bca97a4eb7c3909cd3b5
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 10:26:47 +0500

    added navbar

--- sleeptechnologies/src/App.js
+++ sleeptechnologies/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        hiii 
-      </h1>
+   <>
+   <Navbar/>
+   </>
     </div>
   );
 }
