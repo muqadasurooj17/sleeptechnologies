fe3ade5d1fb352375e661182d1420018cc63c062
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 18:06:10 +0500

    creating cards

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
-
+import card1 from '../images/brands-1-min.jpg';
+import card2 from '../images/brands-2-min.jpg';
+import "../styles/brand.css";
 export default function  Navbar ()
 {
   return (
-    <div>Brand</div>
+    <>
+    <div className='brandcont'>
+      <button className='brandbtn'>
+        Brand</button>
+<div className='row'>
+  <div class="cards" >
+  <img src={myImage} alt="Description of the image" style={{width:"100%", height:"max-content"}}/>
+  
+    <h2>Read More</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisilaborum exceue officia officiis tempore!
+    </p>
+  </div>
+  <div class="cards">
+    <h2>Column 2</h2>
+    <p>Some text..</p>
+  </div>
+    </div>
+    </div>
+    </>
   )
 }
