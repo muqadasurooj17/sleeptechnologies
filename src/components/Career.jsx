65adb8480fd1acf27cb390e8a2e67c80abe2f123
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 23:48:54 +0500

    done with marketting

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -15,7 +15,36 @@ export default function  Brand ()
   Want to make an impact on people’s lives creating the best sleeping <br>
   </br>experience? Join the team — we’re hiring!
   </p>
+
+  
 </div>
+<div className='row'>
+  <div class="widecards" >
+  
+    <h2>Manager</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisilaborum <br>
+      </br>exceue officia officiis tempore!
+    </p>
+  </div>
+  <div class="widecards" >
+  
+    <h2>Outside Sales Representative</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisilaborum <br>
+      </br>exceue officia officiis tempore!
+    </p>
+  </div>
+  <div class="mark">
+<div className='markhead'>
+  Marketting
+</div>
+<div className='markdesc'>
+Open positions in our business team.
+</div>
+ 
+  </div>      
+    </div>
     <hr></hr>
 </>
 
