a0929c98b49c2ac83d1f5f555d438e6b74ee0196
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-20 20:22:49 +0500

    some chnages made

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Career from './components/Career';
 import myImage from './images/pexels-fox-1-min.jpg';
 import Brand from './components/Brand';
+import myImage2 from './images/pexels-fox-2-min.jpg';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 import Mission from './components/Mission';
+import Career from './components/Career';
 
 function App() {
   return (
@@ -21,6 +24,8 @@ function App() {
 <img src={myImage} alt="Description of the image" style={{width:"100%", height:"max-content"}}/>
 <Brand/>
 <Mission/>
+<img src={myImage2} alt="Description of the image" style={{width:"100%", height:"max-content"}}/>
+<Career/>
     </Router>
    </>
     </div>
--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-
-import "../styles/Mission.css";
-export default function  Mission ()
+import "../styles/Career.css";
+export default function  Career()
 {
   return (
     <>
-
 <div className='missioncont'>
 <div className="leftside">
 <div className='brandcont'>
       <button className='brandbtn'>
-        Mission</button></div>
+        Carrers</button></div>
         
         <div className='missionhead'>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam animi eveniet dolorum <br>
@@ -20,20 +18,6 @@ export default function  Mission ()
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam animi eveniet dolorum <br>
 </br> deleniti impedit at modi commodi doloremque.
     </div>
-        </div>
-        <div className='rightside'>
-        <h1>2</h1>
-      <p>
-        Factories
-      </p>
-      <h1>18</h1>
-      <p>
-        Factories
-      </p>
-      <h1>93</h1>
-      <p>
-        Factories
-      </p>
         </div>
     </div>
     
