3cacf11d240467354a06befbe15d71765fe42faa
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-21 11:00:14 +0500

    mission set

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -4,7 +4,39 @@ import "../styles/Mission.css";
 export default function Mission() {
   return (
     <>
-      <div className="missioncont">
+
+<div className="missioncontainer">
+      <div className="left-side">
+      <div className="brandcont">
+            <button className="brandbtn">Mission</button>
+          </div>
+          
+          <div className="missionhead">
+            Our mission is to provide the<br></br> highest quality sleep
+            <br></br> solutions,through innovative<br></br> design and the use
+            of <br></br>advanced materials.
+          </div>
+          <div className="missionsubhead">
+            We strive to exceed customer expectations and improve their{" "}
+            <br></br>overall well-being through better sleep. At Innovative
+            Sleep <br></br>Technologies, we are dedicated to revolutionizing the
+            way<br></br> the world sleeps, one bed at a time
+          </div>
+      </div>
+      <div className="right-side">
+        <p>
+        <h1>2</h1>
+          <p>Factories</p>
+          <h1>18</h1>
+          <p>Factories</p>
+          <h1>93</h1>
+          <p>Factories</p>
+        </p>
+      </div>
+    </div>
+
+
+      {/* <div className="missioncont">
         <div className="leftside">
           <div className="brandcont">
             <button className="brandbtn">Mission</button>
@@ -31,7 +63,7 @@ export default function Mission() {
           <h1>93</h1>
           <p>Factories</p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
